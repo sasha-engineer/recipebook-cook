914b06fb58dae591a9a1fb44aacf395440431003
Oleksandr Stasiuk <ostasiuk@deloitte.com>
2023-03-03 19:31:41 +0200

    fix memory leaks

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,25 @@
+import { Subscription } from 'rxjs';
 import { RecipeService } from './../recipe.service';
 import { Recipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes : Recipe[];
+  recipeChangedSubsctiption: Subscription;
 
   constructor(private recipeService: RecipeService){ }
 
+  ngOnDestroy(): void {
+    this.recipeChangedSubsctiption.unsubscribe();
+  }
+
   ngOnInit(): void {
-    this.recipeService.recipeChanged
+    this.recipeChangedSubsctiption = this.recipeService.recipeChanged
       .subscribe(
         (recipes: Recipe[]) => {
           this.recipes = recipes;
