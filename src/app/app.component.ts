c67e6b37241f51eb83509da82ab7a4ec21e904e8
Oleksandr Stasiuk <ostasiuk@deloitte.com>
2023-03-19 17:08:16 +0200

    + book animation on start page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { isPlatformBrowser } from '@angular/common';
 
@@ -10,15 +10,37 @@ import * as AuthActions from './auth/store/auth.actions';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  isBookLoaded: boolean = false;
+  private bookExpirationTimer: any;
+
   constructor(
     private store: Store<fromApp.AppState>,
     @Inject(PLATFORM_ID) private platformId
   ) { }
 
+  ngOnDestroy(): void {
+    this.clearBookLoaderTimer();
+  }
+
   ngOnInit() {
+    this.setBookLoaderTimer();
+
     if (isPlatformBrowser(this.platformId)) {
       this.store.dispatch(new AuthActions.AutoLogin());
+    }
+  }
+
+  setBookLoaderTimer(expirationDuration: number = 3100) {
+    this.bookExpirationTimer = setTimeout(() => {
+      this.isBookLoaded = true;
+    }, expirationDuration);
+  }
+
+  clearBookLoaderTimer() {
+    if (this.bookExpirationTimer) {
+      clearTimeout(this.bookExpirationTimer);
+      this.bookExpirationTimer = null;
     }
   }
 }
